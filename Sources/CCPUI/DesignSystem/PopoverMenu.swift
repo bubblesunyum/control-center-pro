18d9661baa81699cac9ea2d13d8279f8d7272553
bubbles sandcastle <bubbles@cortico.ai>
2026-09-09 16:39:30 -0500

    give popover section headers 4pt top padding [ccp-axgf]

--- Sources/CCPUI/DesignSystem/PopoverMenu.swift
+++ Sources/CCPUI/DesignSystem/PopoverMenu.swift
@@ -21,6 +21,7 @@ struct PopoverMenuSectionLabel: View {
         Text(title.uppercased())
             .sectionCaps()
             .padding(.horizontal, Space.one)
+            .padding(.top, Space.half)
             .padding(.bottom, Space.quarter)
     }
 }
